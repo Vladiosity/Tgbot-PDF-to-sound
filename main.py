bfd0f262f05b4530d938c2fb673951560f67eef3
xX-VlaDick-Xx <103359637+xX-VlaDick-Xx@users.noreply.github.com>
2023-03-11 00:02:56 +0300

    начал ботика

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-import pdfplumber
-from gtts import gTTS
-from pathlib import Path
 from getmp3 import pdf_tomp3
+import telebot
 
+token = '5839887390:AAFQi0mI1Vw-Mur0sHmPE3kuYpyZaRExLns'
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start_msg(message):
+    bot.send_message(message.chat.id, 'Привет! Я - ботяра, который может "прочитать" глупым роботским голосом текcт, '
+                                  'PDF файл или тектовый файл в формате mp3')
+
+bot.polling(none_stop=True)
 
 def main():
     filename = input('имя файла пж:')
